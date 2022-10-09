1da42d514bc7d9e3e61958e24f1dc0b08ec90365
Karan Motwani <karanmotwani125@gmail.com>
2022-10-09 14:28:05 -0700

    Added ternary operator

--- script.js
+++ script.js
@@ -3,10 +3,5 @@ let hamburger = document.querySelector('.hamburger');
 let nav = document.querySelector('#mob-nav');
 hamburger.addEventListener('click', () => {
     isOpen = !isOpen;
-    if(isOpen) {
-        nav.style.display = "block";
-    }
-    else {
-        nav.style.display = "none";
-    }
+    isOpen ? nav.style.display = "block" : nav.style.display = "none";
 })
